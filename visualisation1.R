21f54d81d89b8fc68599897ec67bea1c1800d7e1
ashsid <ashwinsid6@gmail.com>
2018-11-01 22:20:38 +0530

    Yield_over_years

---
+++ visualisation1.R
@@ -0,0 +1,84 @@
+args <- commandArgs(TRUE)
+statename<-as.character(args[1])  
+districtname<-as.character(args[2])
+data <- read.csv('crop_production.csv')
+data$Yield <- data$Production / data$Area
+state <- as.data.frame(subset(data, data$State_Name == 'Karnataka'))
+district <- subset(state, state$District_Name == "BAGALKOT")
+top5 <- as.data.frame(table(district$Crop))
+top5 <- top5[order(-top5$Freq),]
+top5_crops <- top5[1:5, ]
+crop1 <- as.character(top5_crops[["Var1"]][1])
+crop2 <- as.character(top5_crops[["Var1"]][2])
+crop3 <- as.character(top5_crops[["Var1"]][3])
+crop4 <- as.character(top5_crops[["Var1"]][4])
+crop5 <- as.character(top5_crops[["Var1"]][5])
+library(plotly)
+#year <- as.Date(unique(district$Crop_Year), origin = "1995-1-1")
+year <- as.data.frame(district$Crop_Year)
+crop1_data <- subset(district, district$Crop == crop1)
+crop1data <- subset(crop1_data, crop1_data$Season == 'Kharif')
+crop1data <- as.data.frame(crop1data$Yield)
+
+crop2_data <- subset(district, district$Crop == crop2)
+crop2data <- subset(crop2_data, crop2_data$Season == 'Kharif')
+crop2data <- as.data.frame(crop2data$Yield)
+
+crop3_data <- subset(district, district$Crop == crop3)
+crop3data <- subset(crop3_data, crop3_data$Season == 'Kharif')
+crop3data <- as.data.frame(crop3data$Yield)
+
+crop4_data <- subset(district, district$Crop == crop4)
+crop4data <- subset(crop4_data, crop4_data$Season == 'Kharif')
+crop4data <- as.data.frame(crop4data$Yield)
+
+crop5_data <- subset(district, district$Crop == crop5)
+crop5data <- subset(crop5_data, crop5_data$Season == 'Kharif')
+crop5data <- as.data.frame(crop5data$Yield)
+
+print(crop1)
+
+Sys.setenv("plotly_username"="ashsid98")
+Sys.setenv("plotly_api_key"="AWlVG5DWuZk7AnxAEviR")
+
+
+p <- plot_ly(x = ~year) %>%
+  add_lines(y = ~crop1data$`crop1data$Yield`, name = crop1) %>%
+  add_lines(y = ~crop2data$`crop2data$Yield`, name = crop2, visible = F) %>%
+  add_lines(y = ~crop3data$`crop3data$Yield`, name = crop3, visible = F) %>%
+  add_lines(y = ~crop4data$`crop4data$Yield`, name = crop4, visible = F) %>%
+  add_lines(y = ~crop5data$`crop5data$Yield`, name = crop5, visible = F) %>%
+  layout(
+    title = "Top 5 Best Crops in the Area",
+    xaxis = list(rangeslider = list(type = "date")),
+    
+    yaxis = list(title="Yield"),
+    
+    updatemenus = list(
+      list(
+        buttons = list(
+          list(method = "restyle",
+               args = list("visible", list(TRUE, FALSE,FALSE,FALSE,FALSE)),
+               label = crop1),
+          list(method = "restyle",
+               args = list("visible", list(FALSE,TRUE,FALSE,FALSE,FALSE)),
+               label = crop2),
+          list(method = "restyle",
+               args = list("visible", list(FALSE, FALSE,TRUE,FALSE,FALSE)),
+               label = crop3),
+          list(method = "restyle",
+               args = list("visible", list(FALSE, FALSE,FALSE,TRUE,FALSE)),
+               label = crop4),
+          list(method = "restyle",
+               args = list("visible", list(FALSE, FALSE,FALSE,FALSE,TRUE)),
+               label = crop5)
+          
+          
+        ))))
+options(browser = 'false')
+
+api_create(p, filename = "top-5-crops")
+
+
+
+
